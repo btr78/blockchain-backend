ab03246e81a45be02120a0537b3f924a77e30d90
Bryan R <btsrana@gmail.com>
2025-01-22 13:37:45 -0500

    Update blockchain.js

--- blockchain.js
+++ blockchain.js
@@ -88,6 +88,77 @@ class Transaction {
     this.from = from;
     this.to = to;
     this.amount = amount;
+    this.id = this.generateId();
+    this.signature = null;
+  }
+
+  /**
+   * Generate a unique transaction ID
+   */
+  generateId() {
+    return cryptoRandomString({ length: 64, type: 'hex' });
+  }
+
+  /**
+   * Sign the transaction
+   * @param {string} privateKey - The sender's private key
+   */
+  signTransaction(privateKey) {
+    const signer = crypto.createSign('SHA256');
+    signer.update(this.id).end();
+    this.signature = signer.sign(privateKey, 'hex');
+  }
+
+  /**
+   * Validate the transaction
+   * @param {Blockchain} blockchain - The blockchain instance
+   * @returns {boolean} - Whether the transaction is valid
+   */
+  validate(blockchain) {
+    if (this.amount <= 0) return false;
+    if (blockchain.getBalanceOfAddress(this.from) < this.amount) return false;
+
+    // Verify signature
+    if (!this.signature) return false;
+    const verifier = crypto.createVerify('SHA256');
+    verifier.update(this.id);
+    return verifier.verify(blockchain.getPublicKey(this.from), this.signature, 'hex');
+  }
+}
+
+/**
+ * Smart Contract class
+ */
+class SmartContract {
+  /**
+   * Constructor
+   * @param {Object} terms - The terms of the contract
+   */
+  constructor(terms) {
+    this.terms = terms;
+    this.isFulfilled = false;
+  }
+
+  /**
+   * Execute the smart contract
+   * @param {Blockchain} blockchain - The blockchain instance
+   */
+  execute(blockchain) {
+    if (this.conditionsMet(blockchain)) {
+      this.isFulfilled = true;
+      console.log('Smart contract fulfilled:', this.terms);
+    }
+  }
+
+  /**
+   * Check if conditions are met
+   * @param {Blockchain} blockchain - The blockchain instance
+   * @returns {boolean} - Whether the conditions are met
+   */
+  conditionsMet(blockchain) {
+    // Example condition: check if a certain address has enough balance
+    const { address, minimumBalance } = this.terms;
+    return blockchain.getBalanceOfAddress(address) >= minimumBalance;
   }
 }
 
@@ -104,8 +175,15 @@ class Blockchain {
     this.difficulty = 4;
     this.pendingTransactions = [];
     this.miningReward = 50;
+    this.baseReward = 50;
+    this.bonusReward = 10;
     this.aiLayer = aiLayer;
     this.balances = {};
+    this.keys = {}; // Store public keys for addresses
+    this.governance = {
+      proposals: [],
+      votes: {},
+    };
   }
 
   /**
@@ -149,5 +227,64 @@ class Blockchain {
     console.log(`Mining with difficulty: ${this.difficulty}`);
     block.mineBlock(this.difficulty);
     this.chain.push(block);
-    this.pendingTransactions = [new Transaction(null, rewardAddress, this.miningReward)];
+    this.pendingTransactions = [new Transaction(null, rewardAddress, this.calculateReward())];
   }
+
+  /**
+   * Calculate mining reward
+   * @returns {number} - The calculated reward
+   */
+  calculateReward() {
+    return this.baseReward + (this.chain.length % 10 === 0 ? this.bonusReward : 0);
+  }
+
+  /**
+   * Add a new transaction
+   * @param {Transaction} transaction - The transaction to add
+   */
+  addTransaction(transaction) {
+    if (!transaction.from || !transaction.to) {
+      throw new Error('Transaction must include from and to addresses.');
+    }
+    if (!transaction.validate(this)) {
+      throw new Error('Invalid transaction.');
+    }
+    this.pendingTransactions.push(transaction);
+  }
+
+  /**
+   * Get balance of an address
+   * @param {string} address - The address to query
+   * @returns {number} - The balance of the address
+   */
+  getBalanceOfAddress(address) {
+    return this.chain.reduce((balance, block) => {
+      for (const tx of block.transactions) {
+        if (tx.from === address) balance -= tx.amount;
+        if (tx.to === address) balance += tx.amount;
+      }
+      return balance;
+    }, this.balances[address] || 0);
+  }
+
+  /**
+   * Add a public key for an address
+   * @param {string} address - The address to associate the key with
+   * @param {string} publicKey - The public key
+   */
+  addPublicKey(address, publicKey) {
+    this.keys[address] = publicKey;
+  }
+
+  /**
+   * Get the public key of an address
+   * @param {string} address - The address to query
+   * @returns {string} - The public key of the address
+   */
+  getPublicKey(address) {
+    return this.keys[address];
+  }
+
+  /**
+   * Validate the blockchain
+  
